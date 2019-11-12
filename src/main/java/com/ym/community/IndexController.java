6b8f04db524dfe165b5c1c05e939ba075849061f
WSCYM <2124867925@qq.com>
2019-11-12 20:08:34 +0800

    +bootstrap

--- src/main/java/com/ym/community/IndexController.java
+++ src/main/java/com/ym/community/IndexController.java
@@ -1,16 +1,13 @@
 package com.ym.community;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
+public class IndexController {
 
-    @GetMapping("/hello")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    @GetMapping("/")
+    public String greeting() {
         return "index";
     }
 }
