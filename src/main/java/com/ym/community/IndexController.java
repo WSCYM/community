65d92fd6abb6d08d2dd8b9088056e866c27337f4
WSCYM <2124867925@qq.com>
2019-11-12 20:08:03 +0800

    +bootstrap

--- src/main/java/com/ym/community/IndexController.java
+++ src/main/java/com/ym/community/IndexController.java
@@ -11,6 +11,6 @@ public class HelloController {
     @GetMapping("/hello")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "hello";
+        return "index";
     }
 }
