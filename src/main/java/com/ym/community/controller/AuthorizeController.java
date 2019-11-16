e0bb4d2d4b886d4c45ad8cad2972e8985146369c
WSCYM <2124867925@qq.com>
2019-11-16 16:56:40 +0800

    实现token

--- src/main/java/com/ym/community/controller/AuthorizeController.java
+++ src/main/java/com/ym/community/controller/AuthorizeController.java
@@ -1,4 +1,31 @@
 package com.ym.community.controller;
 
+import com.ym.community.dto.AccessTokenDTO;
+import com.ym.community.dto.GithubUser;
+import com.ym.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("002d06c2619086317c36");
+        accessTokenDTO.setClient_secret("250a1fb7072ea4ec14d237f0970f38d5d33cff72");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8181/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
 }
--- src/main/java/com/ym/community/controller/IndexController.java
+++ src/main/java/com/ym/community/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.ym.community;
+package com.ym.community.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
