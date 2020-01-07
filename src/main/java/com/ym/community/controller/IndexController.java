29f33663ec337c3cbb9091f6f67d0974d16eee76
ym <2124867925@qq.com>
2020-01-07 12:09:03 +0800

    实现主页显示话题列表

--- src/main/java/com/ym/community/controller/IndexController.java
+++ src/main/java/com/ym/community/controller/IndexController.java
@@ -1,13 +1,19 @@
 package com.ym.community.controller;
 
+import com.ym.community.dto.QuestionDTO;
+import com.ym.community.mapper.QuestionMapper;
 import com.ym.community.mapper.UserMapper;
+import com.ym.community.model.Question;
 import com.ym.community.model.User;
+import com.ym.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
@@ -15,8 +21,12 @@ public class IndexController {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private QuestionService questionService;
+
     @GetMapping("/")
-    public String greeting(HttpServletRequest request) {
+    public String greeting(HttpServletRequest request,
+                           Model model) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length!=0) {
             for (Cookie cookie : cookies){
@@ -30,6 +40,10 @@ public class IndexController {
                 }
             }
         }
+
+        List<QuestionDTO> questionList = questionService.list();
+        System.out.println(questionList);
+        model.addAttribute("questions",questionList);
         return "index";
     }
 }
