5f9b6e841f56c89ac68cda49242346bcdc5997ec
ym <2124867925@qq.com>
2020-05-28 23:14:31 +0800

    add interceptor

---
+++ src/main/java/com/ym/community/interceptor/WebConfig.java
@@ -0,0 +1,27 @@
+package com.ym.community.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author ym
+ * @version 1.0.0
+ * @ClassName WebConfig
+ * @Description TODO
+ * @createTime 2020-05-28 22:15:00
+ */
+@Configuration
+//@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private SessionInterceptor sessionInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(sessionInterceptor).addPathPatterns("/**");
+    }
+}
